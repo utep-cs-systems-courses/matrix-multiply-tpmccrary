c9a74d176fd257cbe19bcd9dc24f65823854a34b
tpmccrary <timothypmccrary@gmail.com>
2021-08-29 12:50:46 -0600

    Restructured project, added some python files

---
+++ main.py
@@ -0,0 +1,12 @@
+import matrix_multiply
+
+
+import matrix_multiply.matrixUtils
+
+def main():
+    print("Hello World!")
+    print(matrix_multiply.matrixUtils.genMatrix(32, 0))
+    pass
+
+if __name__ == '__main__':
+    main()
