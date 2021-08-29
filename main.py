d11e19ed5df7c91dd646ff9e78f0daadca2fd576
tpmccrary <timothypmccrary@gmail.com>
2021-08-29 16:29:14 -0600

    Updated print statement with correct values

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     # Submit through github.
     # Use large enough matrices so that it takes around 10s.
 
-    print("Multiplying a 500x500 matrix with values of 5 with another 500x500 matrix with values of 10:")
+    print("Multiplying a 450x450 matrix with values of 5 with another 450x450 matrix with values of 10:")
 
     start = default_timer()
     matrixUtils.printSubarray(MatrixMultiplication.matrix_multiply(matrixUtils.readFromFile("test/matrix_450x450_5"), matrixUtils.readFromFile("test/matrix_450x450_10")))
@@ -25,4 +25,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    
